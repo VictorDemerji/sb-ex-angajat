43f00b425d58c883554376d0de346bf10105a227
VictorDemerji <demerji.v1@gmail.com>
2025-12-06 18:57:33 +0200

    chore: create EmployeeDao interface + create methods

---
+++ src/main/java/pixel/academy/crud_app/dao/EmployeeDAO.java
@@ -0,0 +1,9 @@
+package pixel.academy.crud_app.dao;
+
+import pixel.academy.crud_app.entity.Employee;
+
+public interface EmployeeDAO {
+    double paySalary(Employee employee);
+    void  displayEmployeeInfo(Employee employee);
+    double bonusSalary(Employee employee);
+}
